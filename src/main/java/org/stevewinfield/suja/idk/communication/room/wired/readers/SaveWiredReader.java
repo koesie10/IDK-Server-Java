67b999b5cd5070591a7d6550509a864ef6df18f1
Koen Vlaswinkel <koen@vlaswinkel.info>
2014-09-07 14:12:59 +0200

    Update license in source files

--- src/main/java/org/stevewinfield/suja/idk/communication/room/wired/readers/SaveWiredReader.java
+++ src/main/java/org/stevewinfield/suja/idk/communication/room/wired/readers/SaveWiredReader.java
@@ -1,6 +1,17 @@
 /*
- * IDK Game Server by Steve Winfield
- * https://github.com/WinfieldSteve
+ * Copyright 2014 The IDK Project
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 package org.stevewinfield.suja.idk.communication.room.wired.readers;
 
